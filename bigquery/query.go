1520c3294e096b755e11d80bb7e683f071adad14
Anshul Khandelwal <anshulkhandelwal94@gmail.com>
2023-08-25 19:07:16 +0530

    force storage api use

--- bigquery/query.go
+++ bigquery/query.go
@@ -151,7 +151,7 @@ type QueryConfig struct {
 	JobTimeout time.Duration
 
 	// Force usage of Storage API if client is available. For test scenarios
-	forceStorageAPI bool
+	ForceStorageAPI bool
 }
 
 func (qc *QueryConfig) toBQ() (*bq.JobConfiguration, error) {
@@ -434,7 +434,7 @@ func (q *Query) Read(ctx context.Context) (it *RowIterator, err error) {
 // user's Query configuration.  If all the options set on the job are supported on the
 // faster query path, this method returns a QueryRequest suitable for execution.
 func (q *Query) probeFastPath() (*bq.QueryRequest, error) {
-	if q.forceStorageAPI && q.client.isStorageReadAvailable() {
+	if q.ForceStorageAPI && q.client.isStorageReadAvailable() {
 		return nil, fmt.Errorf("force Storage API usage")
 	}
 	// This is a denylist of settings which prevent us from composing an equivalent
